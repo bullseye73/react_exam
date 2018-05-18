fbc24de9551e5c1e8eadf3b5a67ea08fc762efc5
Claude C. Jeong <claude.c.jeong@selvas.com>
2018-05-18 10:25:54 +0900

    react + webpack tutorial step 1

---
+++ react-fundamentals/webpack.config.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+  entry: {
+    main: './src/index.js',
+  },
+
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015', 'react'],
+        },
+      },
+    ],
+  },
+};
